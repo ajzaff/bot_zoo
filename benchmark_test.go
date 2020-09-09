dd3797272c216f433d2c8e49efc868ce73cacc35
ajzaff <ajzaff@gmail.com>
2020-09-08 20:04:09 -0400

    Add benchmark and take first pprof profile

---
+++ benchmark_test.go
@@ -0,0 +1,15 @@
+package zoo
+
+import "testing"
+
+func BenchmarkJunke11(b *testing.B) {
+	e := NewEngine(1337)
+	p, err := ParseShortPosition("g [rr        c M       r rr  r  E      rHe  D  HrR R   CmRrRR  RRdR]")
+	if err != nil {
+		b.Fatal(err)
+	}
+	e.SetPos(p)
+	for n := 0; n < b.N; n++ {
+		e.GoFixed(8)
+	}
+}
