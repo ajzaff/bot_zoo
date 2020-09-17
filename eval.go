d697131c3fe620964d51af8a8930b83840b187e5
ajzaff <ajzaff@gmail.com>
2020-09-16 21:06:36 -0400

    Cleanup: eval.go

--- eval.go
+++ eval.go
@@ -1,87 +1,44 @@
 package zoo
 
-const (
-	Inf Value = 999
-	Win Value = 1
-)
-
+// Value is a score assigned to a position or move to represent its goodness.
+// Higher numbers are better. Depending on the context Values may range from
+// [0,1] (as in policies) or [-1,1] as in position eval. Inf is provided as
+// value outside the range useful for initialization.
 type Value float64
 
+// Inf is an infinite value outside the range of normal use.
+const Inf Value = 999
+
+// Win represents a winning evaluation in the case of positional eval or an
+// "only move" value in the policy setting.
+const Win Value = 1
+
+// Loss is equal to -Win and represents a losing position in positional eval.
+// It is more canonical than stating "-Win". Loss is not used in policy scoring.
+const Loss Value = -Win
+
+// None returns whether the Value v has valid information. It is functionally
+// the same as `!v.Infinite()``.
+func (v Value) None() bool {
+	return !v.Infinite()
+}
+
+// Infinite returns whether the Value v is at or exceeds either Inf or -Inf.
 func (v Value) Infinite() bool {
 	return v >= Inf || v <= -Inf
 }
 
+// Terminal returns whether the Value v is a Win or Loss value.
 func (v Value) Terminal() bool {
-	return v >= Win || v <= -Win
+	return v.Win() || v.Loss()
 }
 
-func (v Value) Winning() bool {
+// Win returns whether the Value v is a win.
+func (v Value) Win() bool {
 	return v >= Win
 }
 
-func (v Value) Losing() bool {
-	return v <= -Win
-}
-
-func (p *Pos) terminalGoalValue() Value {
-	myGoal, theirGoal := ^NotRank8, ^NotRank1
-	if p.side != Gold {
-		myGoal, theirGoal = theirGoal, myGoal
-	}
-	if p.bitboards[GRabbit.MakeColor(p.side)]&myGoal != 0 {
-		return Win
-	}
-	if p.bitboards[GRabbit.MakeColor(p.side.Opposite())]&theirGoal != 0 {
-		return -Win
-	}
-	return 0
-}
-
-func (p *Pos) terminalEliminationValue() Value {
-	if p.bitboards[GRabbit.MakeColor(p.side.Opposite())] == 0 {
-		return Win
-	}
-	if p.bitboards[GRabbit.MakeColor(p.side)] == 0 {
-		return -Win
-	}
-	return 0
-}
-
-func (p *Pos) immobilized(c Color) bool {
-	for b := p.presence[c]; b > 0; b &= (b - 1) {
-		if !p.Frozen(b.Square()) {
-			return false
-		}
-	}
-	return true
-}
-
-func (p *Pos) terminalImmobilizedValue() Value {
-	if p.immobilized(p.side.Opposite()) {
-		return Win
-	}
-	if p.immobilized(p.side) {
-		return -Win
-	}
-	return 0
-}
-
-func (p *Pos) terminalValue() Value {
-	if v := p.terminalGoalValue(); v != 0 {
-		return v
-	}
-	if v := p.terminalEliminationValue(); v != 0 {
-		return v
-	}
-	if v := p.terminalImmobilizedValue(); v != 0 {
-		return v
-	}
-	return 0
-}
-
-func (p *Pos) Value() Value {
-	if v := p.terminalValue(); v != 0 {
-		return v
-	}
-	return 0
+// Loss returns whether the Value v is a loss.
+func (v Value) Loss() bool {
+	return v <= Loss
 }
